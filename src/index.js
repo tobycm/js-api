ab1f0d9216c8cbed536bd401ef6929785b780b6f
Toby <newtobycm@gmail.com>
2021-12-21 14:27:28 -0800

    upload stuff

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const api = express();
+
+var accesscode;
+
+api.listen(3000, () => {
+  console.log('API up and running!');
+});
+
+api.use(express.static(__dirname + '/public'));
+
+api.get('/join', (req, res) => {
+    accesscode = req.query.code;
+    res.send(accesscode);
+
+    const http = require('http');
+
+    http.get('http://akatsukiducaapi.ddns.net/join?code=' + accesscode, (resp) => {
+    let data = '';
+
+    // A chunk of data has been received.
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+        console.log(JSON.parse(data).explanation);
+    });
+
+    }).on("error", (err) => {
+    console.log("Error: " + err.message);
+    });
+})
+
+
