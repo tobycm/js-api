555ed36a6b2d7c2f209661c8b3f0b55a4556fd00
Toby <newtobycm@gmail.com>
2022-01-21 15:10:30 -0800

    update nhận webhook của facebook

--- src/index.js
+++ src/index.js
@@ -14,21 +14,31 @@ api.get('/join', (req, res) => {
   accesscode = req.query.code;
   res.send('<h1>You are now connected to the server!</h1>');
   const request = require('request');
-  request('http://akatsukiducaapi.ddns.net/join?code=' + accesscode, function (error, response, body) {
-  if (err) { return console.log(err); }
-  });
+  request('http://akatsukiducaapi.ddns.net/join?code=' + accesscode)
 });
 
-api.get('/facebookwebhook', (req, res) => {
-  token = req.query.token;
-  if (token == 'null') {
-    res.send('Access denied');
-  }
-  else {
-    if (token == 'webhookcuafacebookkiabruh')
-    {
-      res.send('Access granted');
-    }
+// Creates the endpoint for our webhook 
+api.post('/webhook', (req, res) => {  
+ 
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
   }
 
 });
