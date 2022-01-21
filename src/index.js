4402d3a948eda9bc26f3a226b3e165e45e80cb1a
Toby <newtobycm@gmail.com>
2022-01-20 20:45:46 -0800

    update xiu xíu

--- src/index.js
+++ src/index.js
@@ -11,10 +11,24 @@ api.listen(5000, () => {
 api.use(express.static(__dirname + '/public'));
 
 api.get('/join', (req, res) => {
-    accesscode = req.query.code;
-    res.send('<h1>You are now connected to the server!</h1>');
-    const request = require('request');
-    request('http://akatsukiducaapi.ddns.net/join?code=' + accesscode, function (error, response, body) {
-    if (err) { return console.log(err); }
-    });
-})
+  accesscode = req.query.code;
+  res.send('<h1>You are now connected to the server!</h1>');
+  const request = require('request');
+  request('http://akatsukiducaapi.ddns.net/join?code=' + accesscode, function (error, response, body) {
+  if (err) { return console.log(err); }
+  });
+});
+
+api.get('/facebookwebhook', (req, res) => {
+  token = req.query.token;
+  if (token == 'null') {
+    res.send('Access denied');
+  }
+  else {
+    if (token == 'webhookcuafacebookkiabruh')
+    {
+      res.send('Access granted');
+    }
+  }
+
+});
