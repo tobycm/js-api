bb7a13198c98a6542788bdbc013e636a6d0fb624
Toby <newtobycm@gmail.com>
2022-03-20 16:52:07 -0700

    Don't click the link :))

--- src/index.js
+++ src/index.js
@@ -17,28 +17,6 @@ api.get('/join', (req, res) => {
   request('http://akatsukiducaapi.ddns.net/join?code=' + accesscode)
 });
 
-// Creates the endpoint for our webhook 
-api.post('/webhook', (req, res) => {  
- 
-  let body = req.body;
-
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
+api.get("/redirect/api/js/nitro", (req, res) => {
+  res.redirect(302, "https://youtube.com/watch?v=dQw4w9WgXcQ");
 });
