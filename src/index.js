b2d34f84afa73c4f33fa90a41f5cbf7dc9934584
Toby <newtobycm@gmail.com>
2022-03-28 15:10:48 -0700

    update api

--- src/index.js
+++ src/index.js
@@ -5,18 +5,9 @@ const api = express();
 var accesscode;
 
 api.listen(5000, () => {
-  console.log('API up and running!');
-});
-
-api.use("/", express.static(__dirname + '/public'));
-
-api.get('/join', (req, res) => {
-  accesscode = req.query.code;
-  res.send('<h1>You are now connected to the server!</h1>');
-  const request = require('request');
-  request('http://akatsukiducaapi.ddns.net/join?code=' + accesscode)
+    console.log('API up and running!');
 });
 
 api.get("/redirect/api/js/nitro", (req, res) => {
-  res.redirect(302, "https://youtube.com/watch?v=dQw4w9WgXcQ");
+    res.redirect(302, "https://youtube.com/watch?v=dQw4w9WgXcQ");
 });
